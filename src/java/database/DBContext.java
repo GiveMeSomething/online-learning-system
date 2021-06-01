5aca47f64a556120431fe4654b7502771eeeedb0
Nabeos <duyanhvu8a4@gmail.com>
2021-06-01 08:18:33 +0700

    refactor: fix dbName

--- src/java/database/DBContext.java
+++ src/java/database/DBContext.java
@@ -19,7 +19,7 @@ public class DBContext {
     //Insert your other code right after this comment
     //Change/update information of your database connection, DO NOT change name of instance variables in this class
     private final String serverName = "localhost";
-    private final String dbName = "db_ite2";
+    private final String dbName = "db_ite1";
     private final String user = "root";
     private final String password = "root"; // password here
 
