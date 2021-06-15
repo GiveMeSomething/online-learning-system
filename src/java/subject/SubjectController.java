01790864afcf747c1239b97e9d0acb0b1b502372
minhhthe150277 <minhhthe150277@fpt.edu.vn>
2021-06-15 23:53:47 +0700

    chore: update project structure (add subject package)

---
+++ src/java/subject/SubjectController.java
@@ -0,0 +1,28 @@
+/**
+ * Jun 15, 2021
+ *
+ * @author Hoang Tien Minh
+ */
+
+package subject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SubjectController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+    }
+}
