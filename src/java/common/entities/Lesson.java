077b73bde0e082bd8d2ad6a4453b56245f686c3a
Nabeos <duyanhvu8a4@gmail.com>
2021-06-16 21:24:09 +0700

    update: add Quiz.java and Lesson.java

--- src/java/common/entities/Lesson.java
+++ src/java/common/entities/Lesson.java
@@ -3,10 +3,129 @@
  *
  * @author Vu Duy Anh
  */
-
 package common.entities;
 
-
 public class Lesson {
+
+    private int id;
+    private String lessonName;
+    private int order;
+    private Status status;
+    private LessonType lessonType;
+    private int courseId;
+    private String videoLink;
+    private String htmlContent;
+    private int quiz_id;
+
+    public Lesson(int id, String lessonName, int order, Status status,
+            LessonType lessonType, int courseId) {
+        this.id = id;
+        this.lessonName = lessonName;
+        this.order = order;
+        this.status = status;
+        this.lessonType = lessonType;
+        this.courseId = courseId;
+    }
+
+    public Lesson(int id, String lessonName, int order, Status status,
+            LessonType lessonType, int courseId, String videoLink, String htmlContent) {
+        this.id = id;
+        this.lessonName = lessonName;
+        this.order = order;
+        this.status = status;
+        this.lessonType = lessonType;
+        this.courseId = courseId;
+        this.videoLink = videoLink;
+        this.htmlContent = htmlContent;
+    }
+
+    public Lesson(int id, String lessonName, int order, Status status,
+            LessonType lessonType, int courseId, String htmlContent, int quiz_id) {
+        this.id = id;
+        this.lessonName = lessonName;
+        this.order = order;
+        this.status = status;
+        this.lessonType = lessonType;
+        this.courseId = courseId;
+        this.htmlContent = htmlContent;
+        this.quiz_id = quiz_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LessonType getLessonType() {
+        return lessonType;
+    }
+
+    public void setLessonType(LessonType lessonType) {
+        this.lessonType = lessonType;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+
+    public String getHtmlContent() {
+        return htmlContent;
+    }
+
+    public void setHtmlContent(String htmlContent) {
+        this.htmlContent = htmlContent;
+    }
+
+    public int getQuiz_id() {
+        return quiz_id;
+    }
+
+    public void setQuiz_id(int quiz_id) {
+        this.quiz_id = quiz_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" + "id=" + id + ", lessonName=" + lessonName + ", order=" + order + ", status=" + status + ", lessonType=" + lessonType + ", courseId=" + courseId + ", videoLink=" + videoLink + ", htmlContent=" + htmlContent + ", quiz_id=" + quiz_id + '}';
+    }
 
 }
---
+++ src/java/common/entities/Quiz.java
@@ -0,0 +1,99 @@
+/**
+ * Jun 16, 2021
+ *
+ * @author Vu Duy Anh
+ */
+package common.entities;
+
+public class Quiz {
+
+    private int id;
+    private String quizName;
+    private int subject_id;
+    private Level quizLevel;
+    private int duration;
+    private int passRate;
+    private TestType quizType;
+    private String description;
+
+    public Quiz(int id, String quizName, int subject_id, Level quizLevel, int duration, int passRate, TestType quizType, String description) {
+        this.id = id;
+        this.quizName = quizName;
+        this.subject_id = subject_id;
+        this.quizLevel = quizLevel;
+        this.duration = duration;
+        this.passRate = passRate;
+        this.quizType = quizType;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public Level getQuizLevel() {
+        return quizLevel;
+    }
+
+    public void setQuizLevel(Level quizLevel) {
+        this.quizLevel = quizLevel;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getPassRate() {
+        return passRate;
+    }
+
+    public void setPassRate(int passRate) {
+        this.passRate = passRate;
+    }
+
+    public TestType getQuizType() {
+        return quizType;
+    }
+
+    public void setQuizType(TestType quizType) {
+        this.quizType = quizType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" + "id=" + id + ", quizName=" + quizName + ", subject_id=" + subject_id + ", quizLevel=" + quizLevel + ", duration=" + duration + ", passRate=" + passRate + ", quizType=" + quizType + ", description=" + description + '}';
+    }
+
+}
